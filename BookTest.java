996212187a5ffa667c0a406ec3e80bf8defe90b0
Michelle <150479177+mmmichelleN@users.noreply.github.com>
2026-05-22 16:00:44 -0400

    Update BookTest.java

--- BookTest.java
+++ BookTest.java
@@ -95,15 +95,11 @@ class BookTest {
 		assertEquals("Anthony Verity", newBook.getTranslator());	
 		assertEquals("Title: The Odyssey, Author: Homer, Year Published: 2016, IBSN: 9780198736479, Translated: true, Translator: Anthony Verity", newBook.bookToString(newBook));		
 	
-		newBook.setTitle("Roman Lives");
-		newBook.setAuthor("Plutarch");
-		
-	
 	}
 
 	@Test
 	void test6() {
-		Book newBook = new Book(); //create a new Book object
+		Book newBook = new Book(); //create 1 book object
 		newBook.setTitle("Gideon the Ninth"); //set the book title
 		newBook.setAuthor("Tamsyn Muir"); //set the author
 		newBook.setIBSN("9781250313188");
@@ -116,7 +112,7 @@ class BookTest {
 		assertEquals(2019, newBook.getYearPublished());
 		assertEquals(false, newBook.getTranslated());
 		
-		Book newBook1 = new Book(); //create another book instance
+		Book newBook1 = new Book(); //create a 2nd book object
 		newBook1.setTitle("Harrow the Ninth"); 
 		newBook1.setAuthor("Tamsyn Muir");
 		newBook1.setIBSN("9781250313218");
@@ -129,12 +125,51 @@ class BookTest {
 		assertEquals(2020, newBook1.getYearPublished());
 		assertEquals(false, newBook1.getTranslated());
 		
+		Book newBook2 = new Book(); //create a 3rd book object
+		newBook2.setTitle("Nona the Ninth");
+		newBook2.setAuthor("Tamsyn Muir");
+		newBook2.setIBSN("9781250899132");
+		newBook2.setYearPublished(2022);
+		newBook2.setTranslated(false);
+		
+		assertEquals("Nona the Ninth", newBook2.getTitle());
+		assertEquals("Tamsyn Muir", newBook2.getAuthor());
+		assertEquals("9781250899132", newBook2.getIBSN());
+		assertEquals(2022, newBook2.getYearPublished());
+		assertEquals(false, newBook2.getTranslated());
+		
 		BookCollection newBookCollection = new BookCollection(); //make a new collection of books 
 		
 		newBookCollection.addBook(newBook); //add Gideon the Ninth to collection
 		newBookCollection.addBook(newBook1); //add Harrow the Ninth to collection
+		newBookCollection.addBook(newBook2); //add Nona the Ninth to collection
 		
-		assertEquals("Gideon the Ninth, Harrow the Ninth", newBookCollection.getBookCollection());
+		assertEquals(3, newBookCollection.numBooks());
+		
+		assertEquals("Gideon the Ninth, Harrow the Ninth, Nona the Ninth", newBookCollection.getBookTitles());
 	}
+	
+	@Test
+	void test7()
+	{
+		TranslatedBook newBook = new TranslatedBook("The Odyssey", "Homer", 2016, "9780198736479", true, "Anthony Verity");
+		TranslatedBook newBook1 = new TranslatedBook("Roman Lives", "Plutarch", 2008, "9780199537389", true, "Robin Waterfield");
+		
+		BookCollection newBookCollection = new BookCollection();
+		
+		newBookCollection.addBook(newBook);
+		newBookCollection.addBook(newBook1);
+		
+		assertEquals(2, newBookCollection.numBooks());
+		assertEquals("The Odyssey, Roman Lives", newBookCollection.getBookTitles());
+ 	
+		Book newBook2 = new Book("Gideon the Ninth", "Tamsyn Muir", "9781250313188", 2019, false);
+		newBookCollection.addBook(newBook2);	
+		
+		assertEquals("The Odyssey, Roman Lives, Gideon the Ninth", newBookCollection.getBookTitles());
+		
+	}
+	
+	
 
 }
